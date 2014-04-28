f6b957e485d220d39b6929bf2a940648f4664d07
Jan-Erik Revsbech <janerik@moc.net>
2014-04-28 15:03:07 +0200

    Added PHP module

---
+++ modules/php/manifests/params.pp
@@ -0,0 +1,38 @@
+# == Class: php::params
+#
+# PHP params class
+#
+# Configuration class for php module
+#
+# === Parameters
+#
+# No parameters
+#
+# === Variables
+#
+# [*ensure*]
+#   The PHP ensure of PHP to install
+#
+# === Examples
+#
+#  include php::dev
+#
+# === Authors
+#
+# Christian "Jippi" Winther <jippignu@gmail.com>
+#
+# === Copyright
+#
+# Copyright 2012-2013 Christian "Jippi" Winther, unless otherwise noted.
+#
+class php::params {
+
+  $ensure = 'installed'
+
+  $config_root = '/etc/php5'
+
+  $config_root_ini = "${::php::params::config_root}/mods-available"
+
+  $augeas_contrib_dir = '/usr/share/augeas/lenses/contrib'
+
+}
