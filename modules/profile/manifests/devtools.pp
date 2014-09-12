66ceb2d607f91e4ad3accd5c02f93c475b553d0e
Jan-Erik Revsbech <janerik@moc.net>
2014-09-12 10:45:53 +0200

    [TASK] Make new compass versions available

--- modules/profile/manifests/devtools.pp
+++ modules/profile/manifests/devtools.pp
@@ -1,21 +1,26 @@
 class profile::devtools {
 
-	$devpackagelist = [
-		"ruby-compass"
-	]
 
 	$devBackportsPackagelist = [
 		"git-man",
 		"git"
 	]
 
-	package { $devpackagelist:
-		ensure => present,
-	}
-
 	apt::force { $devBackportsPackagelist:
 	  release => 'wheezy-backports',
 	  require => Apt::Source['backports'],
+	}
+
+	package { 'compass':
+		ensure   => 'installed',
+		provider => 'gem',
+		install_options => '--pre'
+	}
+
+	package { 'sass':
+		ensure   => 'installed',
+		provider => 'gem',
+		install_options => '--pre'
 	}
 
 }
