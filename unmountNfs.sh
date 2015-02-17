1e2a8c6c551d1448ffc90de3843b5f621e2fb62f
Jon Klixbüll Langeland <jon@klixbull.org>
2015-02-17 16:06:51 +0100

    [TASK] Add check for mountpoint before running umount

--- unmountNfs.sh
+++ unmountNfs.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-sudo umount $DIR/workdir
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/workdir"
+
+if mount | grep $dir > /dev/null; then
+    echo "Unmounting: $dir"
+	sudo umount $dir
+else
+    echo "$dir not mounted."
+fi
